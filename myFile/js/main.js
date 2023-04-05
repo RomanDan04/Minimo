07b8d2387c3be72e284920233161dbe261f18e14
Roman Dan <roman2000dan2000@gmail.com>
2023-04-05 19:38:40 +0300

    add code

---
+++ myFile/js/main.js
@@ -0,0 +1,14 @@
+function extinde(element)
+{
+    var x = document.getElementById("mySpan");
+    x.style.display = "inline-block";
+    var y=document.getElementById("hide1");
+    var z=document.getElementById("hide2");
+    y.style.display="inline-block";
+    z.style.display="inline-block";
+}
+function term()
+{
+    var x = document.getElementById("mySpan");
+    x.style.display = "none";
+}
